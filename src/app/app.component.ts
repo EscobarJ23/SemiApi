0881284f91edc038a78bd3f4ddd05bbf43a98f9a
EscobarJ23 <jorge.escobar.jimenez@udla.edu.ec>
2020-10-17 08:26:54 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  google: any;
+  title = 'SemiApi';
+  lat = 51.678418;
+  lng = 7.809007;
+}
